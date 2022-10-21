06c8b07e3463d82d1e62ca0556abfc269781855f
andreaa21 <andrea.zollini@proton.me>
2022-10-21 17:18:01 +0200

    add grid to main

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 **BONUS:**
 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
 ****2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
-**Consigli del giorno:** :party_wizard:
+**Consigli del giorno:** 
 ****Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
 Ad esempio:
 Di cosa ho bisogno per generare i numeri?
@@ -27,3 +27,91 @@ Le validazioni e i controlli possiamo farli anche in un secondo momento.
 */ 
 
 
+/*
+    COSE DA FARE
+
+    - scelgo la difficoltà
+
+
+    - PLAY    
+        - generare la griglia quadrata in base alla difficoltä scelta
+            - quando genero la griglia genero anche le bombe
+
+        - inserire le celle dentro la griglia
+
+        - inserire il numero dentro le celle
+
+        - creare le bombe
+
+    - al CLICK della cella
+        - verificare se è una bomba ----- FINE GIOCO
+        - la cella si colora
+
+    - termina la partita se ho cliccato su una bomba o se ho finito i tentativi
+        -stampo il messaggio di fine partita
+        - accendo tutte le bombe 
+        - congelo la griglia
+
+    - RESET
+        - cancello la vecchia griglia
+        - cancello le bombe
+*/
+
+
+const main = document.querySelector('.main-wrapper');
+const btn = document.querySelector('#button');
+const levelChoice = document.querySelector ('#level'); 
+const bombNumber = 16;
+
+
+btn.addEventListener('click', play);
+
+
+function play(){
+    // stabilisco il numero di square in base alla difficoltà
+     const squareNumber = levelChoice.value;
+
+    reset();
+
+    // genero la griglia
+    createGrid(squareNumber);
+
+}
+
+
+function createGrid(squareNumber){
+    // creo la griglia
+    const grid = document.createElement('div');
+    // gli aggiungo la classe grid
+    grid.className = 'grid';
+    // aggiungo le classi che servono
+    grid.classList.add('d-flex', 'flex-wrap', 'align-items-center');
+
+    // creo gli square
+    for(let i = 1; i <= squareNumber; i++){
+        const square = createSquare(i, squareNumber);
+        grid.append(square);
+    }
+    // stampo la griglia nel main
+    main.append(grid);
+}
+
+function createSquare(squareId, squareNumber){
+    // creo lo square
+    const square = document.createElement('div');
+    // gli do la classe square
+    square.className = 'square';
+    // gli aggiungo le classi che mi servono
+    square.classList.add('d-flex', 'align-items-center', 'justify-content-center', 'square'+squareNumber )
+    // creo l' id custom
+    square.squareId = squareId;
+    // scrivo l'id custom dentro lo square
+    square.innerHTML = `${squareId}`; 
+    // restituisco lo square
+    return square;
+}
+
+function reset(){
+    // cancello la vecchia griglia 
+    main.innerHTML = '';
+}
